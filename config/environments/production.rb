6f04358c4b649bc0c681a809f71b80b2dac301eb
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2023-02-14 14:07:14 -0300

    link do config.hosts atualizado no production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "manegement-production.up.railway.app"
+  config.hosts << "management-production-1303.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
