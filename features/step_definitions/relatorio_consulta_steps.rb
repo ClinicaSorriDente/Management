b78cefa61117e75682bf4d0abbb818ae70c9bf57
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2023-03-22 00:37:13 -0300

    correçoes do cucumber de relatorio consulta

--- features/step_definitions/relatorio_consulta_steps.rb
+++ features/step_definitions/relatorio_consulta_steps.rb
@@ -56,9 +56,9 @@ end
 
 Given('eu estou na pagina de criar relatorio_consulta') do
   visit 'pacientes/new'
-  fill_in 'paciente[nome]', with: 'Rodrigo Santos'
+  fill_in 'paciente[nome]', with: 'Rodrigo pereira'
   fill_in 'paciente[nascimento]', with: "2001-04-25"
-  fill_in 'paciente[cpf]', with: '38854324124'
+  fill_in 'paciente[cpf]', with: '49949950449'
   fill_in 'paciente[telefone]', with: '87998165631'
   fill_in 'paciente[email]', with: 'rodrigo.leandro.zx@gmail.com'
   fill_in 'paciente[endereco_paciente_attributes][cep]', with: '55298165'
@@ -84,11 +84,11 @@ Given('eu estou na pagina de criar relatorio_consulta') do
   visit '/consulta/new'
   fill_in 'consultum[data]', with: '22/03/2023'
   fill_in 'consultum[horario]', with: '12:41'
-  select 'Rodrigo Santos', from: 'consultum[paciente_id]'
+  select 'Rodrigo pereira', from: 'consultum[paciente_id]'
   select 'michel', from: 'consultum[dentistum_id]'
   click_button 'Criar Consultum'
   visit '/consulta'
-  expect(page).to have_content('Rodrigo Santos')
+  expect(page).to have_content('Rodrigo pereira')
 
   visit '/consulta/1/relatorio_consulta/new'
 
@@ -164,7 +164,6 @@ end
 
 When('eu preecho um relatorio_consulta com a checkbox de presença marcada') do
   check 'relatorio_consultum[presente]'
-  fill_in 'relatorio_consultum[comentario]', with: " "
 end
 
 When('eu preecho um relatorio_consulta com comentario {string} e marco a checkbox de presença') do |comentario|
